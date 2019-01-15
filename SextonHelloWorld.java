bf71915a3249bb1a4a86cbbd06ec548a3b92e48b
DarkImposter <psexton@cfsnc.org>
2019-01-14 19:10:52 -0500

    first class

---
+++ SextonHelloWorld.java
@@ -0,0 +1,13 @@
+// Auther: Parker Sexton
+// Date: 1/14/19
+// Purpose: learn to print text to the console
+public class SextonHelloWorld {
+
+	public static void main(String[] args) {
+		// code to print to the console
+		System.out.println("Hello World!");
+		System.out.println("Welcome to Java Paker!");
+
+	}
+
+}
